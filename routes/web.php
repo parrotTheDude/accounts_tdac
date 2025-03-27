2aa3ed6d0ced2f712dc24327a3510643eead7423
Jacob <program.acquaint533@passinbox.com>
2025-03-27 16:50:28 +1100

    Route updates

--- routes/web.php
+++ routes/web.php
@@ -29,17 +29,15 @@ Route::middleware(['auth'])->group(function () {
     Route::view('/users', 'users.index')->name('users.index');
     Route::view('/subscriptions', 'subscriptions.index')->name('subscriptions.index');
 
-    // Email Routes
     Route::prefix('emails')->name('emails.')->group(function () {
         Route::get('/', [EmailController::class, 'index'])->name('index');
+        Route::get('/history', [\App\Http\Controllers\BulkEmailController::class, 'index'])->name('history');
         Route::get('/progress', [EmailController::class, 'bulkProgress'])->name('progress');
-
+    
+        // ⚠️ Catch-all must come last
         Route::get('{templateId}', [EmailController::class, 'show'])->name('show');
         Route::get('{templateId}/send', [EmailController::class, 'sendForm'])->name('sendForm');
         Route::post('{templateId}/send', [EmailController::class, 'sendBulk'])->name('sendBulk');
         Route::post('{templateId}/send-test', [EmailController::class, 'sendTest'])->name('sendTest');
     });
-
-    Route::get('/emails/history', [\App\Http\Controllers\BulkEmailController::class, 'index'])
-    ->name('emails.history');
 });
