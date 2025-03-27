561bcbd93ff6a8e00bfaa7df977725c46fe7a485
Jacob <program.acquaint533@passinbox.com>
2025-03-27 15:39:36 +1100

    Moved password div

--- resources/views/emails/send-form.blade.php
+++ resources/views/emails/send-form.blade.php
@@ -29,12 +29,13 @@
   <div>
     <label class="block font-medium text-gray-700 mb-1">Confirm Your Password</label>
     <input type="password" name="password" required class="w-full border rounded-md px-3 py-2">
+  </div>
+
     @if ($errors->any())
     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
         <strong>Error:</strong> {{ $errors->first() }}
     </div>
     @endif
-  </div>
 
   <button type="submit"
           class="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-md font-medium">
