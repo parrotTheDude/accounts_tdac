afbb385bec22c73fc9f97c46ad8b640652346a27
Jacob <program.acquaint533@passinbox.com>
2025-03-28 14:07:48 +1100

    Participant link update

--- app/Http/Controllers/ParticipantLinkController.php
+++ app/Http/Controllers/ParticipantLinkController.php
@@ -15,7 +15,10 @@ class ParticipantLinkController extends Controller
             abort(403, 'Only participants can have links.');
         }
 
-        $users = User::whereIn('user_type', ['parent', 'support_coordinator', 'external'])->get();
+        // Get linkable users
+        $users = User::whereIn('user_type', ['parent', 'support_coordinator', 'external'])
+                    ->whereNull('archived_at')
+                    ->get();
 
         return view('participant_links.create', compact('participant', 'users'));
     }
@@ -24,13 +27,13 @@ class ParticipantLinkController extends Controller
     {
         $validated = $request->validate([
             'related_user_id' => ['required', 'exists:users,id'],
-            'relationship' => ['required', 'in:parent,support_coordinator'],
+            'relationship'    => ['required', 'in:parent,support_coordinator'],
         ]);
 
         ParticipantLink::create([
-            'participant_id' => $participant->id,
-            'related_user_id' => $validated['related_user_id'],
-            'relationship' => $validated['relationship'],
+            'participant_id'   => $participant->id,
+            'linked_user_id'   => $validated['related_user_id'], // new correct field name
+            'relation'         => $validated['relationship'],    // use 'relation' matching your DB column
         ]);
 
         return redirect()->route('users.edit', $participant)->with('success', 'Link added.');
